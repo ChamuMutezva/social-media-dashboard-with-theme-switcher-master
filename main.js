fd31c8cd3ed20a542667d378a3ac0f5b0108988e
Chamunorwa Mutezva <40114498+ChamuMutezva@users.noreply.github.com>
2020-04-14 23:49:34 +0200

    feat: cards primary light - dark mode theme change

--- main.js
+++ main.js
@@ -1,11 +1,20 @@
 const switchCtl = document.querySelector(".slider");
 const themeTextCtl = document.querySelector(".themeTextCtl");
 const bodyTag = document.querySelector("body");
-console.log(bodyTag);
+const headerTag = document.querySelector("header");
+const primaryCard = Array.from(document.querySelectorAll(".primary__card"));
+
+console.log(primaryCard);
 
 switchCtl.addEventListener("click", () => {
-    console.log("Toggle button clicked")
-    bodyTag.classList.toggle("bg-background-light")
+    console.log("Toggle button clicked");
+    bodyTag.classList.toggle("bg-background-light");
+    headerTag.classList.toggle("bg-header-background");
+
+    primaryCard.forEach(card => {
+        card.classList.toggle("bg-card-background-light");
+    })
+    
     console.log(themeTextCtl.innerHTML)
     if(themeTextCtl.innerHTML == "Dark") {
         themeTextCtl.innerHTML = "Light" 
